bfe2a9e4c93cec9625c28470fbcb4074de5f29f3
hakanlsk <hakanlsk20@gmail.com>
2024-07-04 16:24:17 +0300

    db start

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+//db
+import db from "./_db";
+
+//types
+import { typeDefs } from "./schema";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+  },
+};
+
 //server setup
 const server = new ApolloServer({
-  //typedefs
-  //resolvers
+  typeDefs,
 });
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
